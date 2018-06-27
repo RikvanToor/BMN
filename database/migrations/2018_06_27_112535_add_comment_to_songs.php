3a7f0e402a4bf850d92d905dd2b354b8c4e72d19
Rik van Toor <rik.van.toor@gmail.com>
2018-06-27 13:42:58 +0200

    Added songs and users tables

--- database/migrations/2018_06_27_112535_add_comment_to_songs.php
+++ database/migrations/2018_06_27_112535_add_comment_to_songs.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSongsTable extends Migration
+class AddCommentToSongs extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateSongsTable extends Migration
      */
     public function up()
     {
-        Schema::create('songs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->string('artist');
-            $table->string('spotify_link');
-            $table->timestamps();
+        Schema::table('songs', function (Blueprint $table) {
+            $table->string('comment');
         });
     }
 
@@ -29,6 +25,8 @@ class CreateSongsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('songs');
+        Schema::table('songs', function (Blueprint $table) {
+            $table->dropColumn('comment');
+        });
     }
 }
