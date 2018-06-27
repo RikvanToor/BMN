5c17fcf4ee2092ffd8868666d40bca3b0eade521
Rik van Toor <rik.van.toor@gmail.com>
2018-06-27 15:07:26 +0200

    Added unique constraints on username and email

---
+++ database/migrations/2018_06_27_122726_add_constraints_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConstraintsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unique('username');
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique('users_username_unique');
+            $table->dropUnique('users_email_unique');
+        });
+    }
+}
