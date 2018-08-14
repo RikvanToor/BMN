e4d8702ad6fc5d3a34d254b86f2108eacea18381
Rik van Toor <rik.van.toor@gmail.com>
2018-08-14 22:40:32 +0200

    Changed carousel to react-bootstrap

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import bg1  from "../images/bg_1.jpg";
-import bg2  from "../images/bg_2.jpg";
-import bg3  from "../images/bg_3.jpg";
+import bg1 from "../images/bg_1.jpg";
+import bg2 from "../images/bg_2.jpg";
+import bg3 from "../images/bg_3.jpg";
 import logo from "../images/logo.png";
+import CarouselComp from 'react-bootstrap/lib/Carousel';
 
 class Carousel extends Component {
   constructor(props) {
@@ -11,50 +12,17 @@ class Carousel extends Component {
   }
   render() {
     return (
-      <div
-        id="carouselExampleIndicators"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <ol className="carousel-indicators">
-          <li
-            data-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            className="active"
-          />
-          <li data-target="#carouselExampleIndicators" data-slide-to="1" />
-          <li data-target="#carouselExampleIndicators" data-slide-to="2" />
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img className="d-block w-100" src={bg1} alt="First slide" />
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src={bg2} alt="Second slide" />
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src={bg3} alt="Third slide" />
-          </div>
-        </div>
-        <a
-          className="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+      <CarouselComp>
+        <CarouselComp.Item>
+          <img className="d-block w-100" src={bg1} alt="First slide" />
+        </CarouselComp.Item>
+        <CarouselComp.Item>
+          <img className="d-block w-100" src={bg2} alt="First slide" />
+        </CarouselComp.Item>
+        <CarouselComp.Item>
+          <img className="d-block w-100" src={bg3} alt="First slide" />
+        </CarouselComp.Item>
+      </CarouselComp>
     );
   }
 }
