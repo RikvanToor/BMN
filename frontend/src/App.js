9ff471ee814bdff00bc3917c143939bfcbab4a4f
Rik van Toor <rik.van.toor@gmail.com>
2018-11-11 01:57:40 +0100

    Re-added CSS that was accidentally removed from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Wrapper from './components/wrapper.jsx';
 import UserStore from '@Stores/UserStore.js';
 import { dispatch } from '@Services/AppDispatcher.js';
 import { checkLoginAction } from '@Actions/UserActions.js'
+import CSS from './App.css';
 
 /*
  * Main entry point of the BMN frontend.
