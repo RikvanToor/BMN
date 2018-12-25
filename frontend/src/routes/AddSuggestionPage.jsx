d041cd8a20f81420d68718368d397ec40ef5e454
Diego <d.p.consen@students.uu.nl>
2018-12-25 13:47:39 +0100

    Modal toegevoegt als feedback voor formulier

--- frontend/src/routes/AddSuggestionPage.jsx
+++ frontend/src/routes/AddSuggestionPage.jsx
@@ -1,6 +1,6 @@
 //UI imports
 import React, {Component} from 'react';
-import {PageHeader} from 'react-bootstrap';
+import {Table, Label, PageHeader, Button, ButtonGroup, Modal, Panel} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import SongForm from '@Components/SongForm.jsx';
 
@@ -21,21 +21,60 @@ class AddSuggestionPage extends Component{
         this.state = {
             modal: false
         }
-    }
 
+        //Bound callback
+        this.clearModal = this.clearModal.bind(this);
+    }
+    clearModal(){
+        this.setState({modal:false});
+    }
     /**
      * Handles creation of a new suggestion
      * @param {Song} song The song object that was created 
      */
     handleNewSong(song){
         deferredDispatch(addSongAction(song.toJS()));
+        let modal = {
+            body: ( <React.Fragment>
+                    <p>Je suggestie is nu toegevoegt aan de suggestielijst, de commissie zal er bij de volgende vergadering naar luisteren!</p>
+                    </React.Fragment>),
+            title: 'Hoera!',
+            onAccept: this.clearModal
+        };
+        this.setState({modal:modal});
+    }
+     /**
+     * Displays a modal
+     * @param {string} title 
+     * @param {React element} body 
+     * @param {function} onAccept 
+     */
+    displayModal(title, body, onAccept){
+        return (
+            <Modal show={true}>
+                <Modal.Header>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    {body}
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button bsStyle="primary" onClick={onAccept}>OK!</Button>
+                </Modal.Footer>
+            </Modal>
+        );
     }
 
     render(){
+        let modal = this.state.modal;
+
         return(
             <div>
                 <PageHeader>Suggestie toevoegen</PageHeader>
                 <SongForm song={new Song()} onSave={this.handleNewSong}/>
+                {modal? this.displayModal(modal.title,modal.body, modal.onAccept) : null}
             </div>
         );
     }
