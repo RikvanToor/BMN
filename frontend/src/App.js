026413749cd26f7dfae7c76ef77f4759a94cd032
Rik van Toor <rik.van.toor@gmail.com>
2019-02-09 14:26:10 +0100

    Systeem voor een losse publieke pagina

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import {Container} from 'flux/utils';
+import { Container } from 'flux/utils';
 import Wrapper from './components/wrapper.jsx';
+import Public from './components/public.jsx';
 import UserStore from '@Stores/UserStore.js';
 import { dispatch } from '@Services/AppDispatcher.js';
 import { checkLoginAction } from '@Actions/UserActions.js'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CSS from './App.css';
 
 /*
@@ -27,7 +29,14 @@ class App extends Component {
   }
 
   render() {
-    return <Wrapper ReadyToRender={this.state.doneFetchingUser} user={this.state.user} />
+    return <BrowserRouter>
+      <Route path='*'>
+        <Switch>
+          <Route key='public' exact path='(|/public)' render={() => <Public />} />
+          <Route key='private' path='*' render={() => <Wrapper ReadyToRender={this.state.doneFetchingUser} user={this.state.user} />} />
+        </Switch>
+      </Route>
+    </BrowserRouter>;
   }
 }
 
---
+++ frontend/src/components/public.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Public extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Dit is een test</h1>
+      </div>
+    );
+  }
+}
+
+export default Public;
