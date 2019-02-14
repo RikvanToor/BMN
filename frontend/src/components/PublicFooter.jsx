24e2adf1ec3d98d6697612989943c45cf74d4614
Atleast <d.p.consen@students.uu.nl>
2019-02-14 12:05:28 +0100

    Pagina soort van af, geen functionaliteit navbar

--- frontend/src/components/PublicFooter.jsx
+++ frontend/src/components/PublicFooter.jsx
@@ -9,9 +9,12 @@ class PublicFooter extends Component {
     return (
       <div className="footer-logos">       
         <div className="row row-no-gutters">      
-          <img className="col-sm-4" src={Logo1} />
-          <img className="col-sm-4" src={Logo2} />
-          <img className="col-sm-4" src={Logo3} />
+          <a href="https://www.dehelling.nl/" target="_blank"><img className="col-sm-3 sponsor align-middle" src={Logo1} /></a>
+          <a href="https://www.uu.nl/organisatie/alumni/utrechts-universiteitsfonds" target="_blank"><img className="col-sm-3 sponsor align-middle" src={Logo2} /></a>
+          <a href="https://www.a-eskwadraat.nl/Home" target="_blank">  <img className="col-sm-3 sponsor align-middle" src={Logo3} /></a>
+        </div>
+        <div className="pt-2">
+          <h3 className="text-center">© 2019 BÈTA MUSIC NIGHT</h3>
         </div>
       </div>
     );
