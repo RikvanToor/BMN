2eb321bb40f95781a007421320047a0831d59aa8
Atleast <d.p.consen@students.uu.nl>
2019-03-03 21:40:34 +0100

    Visual updates complete
    Kan nog wat opschoning gebruiken, maar dit werkt iig.

--- frontend/src/components/PublicFooter.jsx
+++ frontend/src/components/PublicFooter.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Logo1 from "../images/helling.png";
 import Logo2 from "../images/u-fonds.jpg";
 import Logo3 from "../images/a-eslogo.png";
-import { Table, Tooltip, OverlayTrigger, Button, ButtonGroup, PageHeader } from 'react-bootstrap';
 
 class PublicFooter extends Component {
   componentDidMount(){
@@ -15,15 +14,18 @@ class PublicFooter extends Component {
   }
 
   render() {
-    return (
-      <div className="footer-logos">       
-        <div className="row row-no-gutters">      
-          <a href="https://www.dehelling.nl/" target="_blank"><img id="l1" className="col-sm-3 sponsor align-middle" /></a>
-          <a href="https://www.uu.nl/organisatie/alumni/utrechts-universiteitsfonds" target="_blank"><img id="l2" className="col-sm-3 sponsor align-middle" /></a>
-          <a href="https://www.a-eskwadraat.nl/Home" target="_blank">  <img id="l3" className="col-sm-3 sponsor align-middle" /></a>
+    return (   
+      <div>
+        <div className="row row-no-gutters text-center"> 
+          <div className="d-inline-block">
+          <a href="https://www.dehelling.nl/" target="_blank"><img id="l1" className="sponsor" /></a>
+          <a href="https://www.uu.nl/organisatie/alumni/utrechts-universiteitsfonds" target="_blank"><img id="l2" className="sponsor-uu" /></a>
+          <a href="https://www.a-eskwadraat.nl/Home" target="_blank">  <img id="l3" className="sponsor" /></a>
+          </div>
         </div>
-        <div className="pt-2">
-          <h3 className="text-center">© 2019 BÈTA MUSIC NIGHT</h3>
+        <div className="text-center">
+          <h3>&copy; Bèta Music Night 2019</h3>
+          <a href="https://www.betamusicnight.nl/login">Deelnemers</a> - <a href="mailto:bmn@a-eskwadraat.nl">Contact</a>
         </div>
       </div>
     );
