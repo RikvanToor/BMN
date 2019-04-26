3b4932c01514ff0d6893fa74a11950f77aebf19d
Rik van Toor <rik.van.toor@gmail.com>
2019-04-26 14:27:27 +0200

    [HOTFIX] Updated BMNInfo

--- frontend/src/components/BMNInfo.jsx
+++ frontend/src/components/BMNInfo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { PageHeader, Grid, Col } from 'react-bootstrap';
+import { PageHeader, Grid, Col, Button } from 'react-bootstrap';
 
 class BMNInfo extends Component {
   render() {
@@ -8,18 +8,28 @@ class BMNInfo extends Component {
         <PageHeader>Wat is de Bèta Music Night?</PageHeader>
         <Col sm={10} xs={12} className="col-sm-offset-1">
           <p>
-            De Bèta Music Night (BMN) is een jaarlijks terugkerend concert georganiseerd door de gelijknamige commissie van studievereniging A–Eskwadraat.
+            De Bèta Music Night (BMN) is een jaarlijks terugkerend concert, georganiseerd door de gelijknamige commissie van studievereniging A–Eskwadraat.
             De eerste editie was in 2013, en hierop volgden al zés edities. Ondertussen is de Bèta Music Night uitgegroeid tot één van de grootste activiteiten van A–Eskwadraat.
             We zijn alweer druk bezig met het organiseren van de zevende editie, en tevens het vijfjarige jubileum van de commissie.
           </p>
           <p>
             Voor de BMN worden de beste muzikanten van A–Eskwadraat bij elkaar gebracht. Ook dit jaar hebben we weer een groep van dertig muzikanten bij elkaar weten te vinden.
-            De setlist bestaat volledig uit covers van bekende en wat minder bekende nummers. De bezetting per nummer is steeds anders, zo staat er bij geen elk nummer precies
+            De setlist bestaat volledig uit covers van bekende en wat minder bekende nummers. De bezetting per nummer is steeds anders, zo staat er bij geen enkele twee nummers precies
             dezelfde groep op het podium. Op deze manier leren de muzikanten samenspelen met veel verschillende mensen en doen hierbij heel nuttige bandervaring op. Het is ook
-            mooi om te zien dat wij als echte bèta-vereniging naast onze exacte kwaliteiten ook ons van een een andere kant kunnen laten zien.
+            mooi om te zien dat wij als echte bèta-vereniging naast onze exacte kwaliteiten ons ook van een een andere kant kunnen laten zien.
           </p>
           <p>
-            De zevende editie van de BMN vindt plaats op 5 juni 2019 in de Helling, Utrecht. Houdt onze <a href="https://www.facebook.com/BetaMusicNight/" target="_blank">Facebookpagina</a> goed in de gaten voor de laatste nieuwtjes.
+            De zevende editie van de BMN vindt plaats op 5 juni 2019 in de Helling, Utrecht. 
+            De deuren zullen om 19:30 openen, en om 20:00 begint het concert.
+            Houd onze <a href="https://www.facebook.com/BetaMusicNight/" target="_blank">Facebookpagina</a> goed in de gaten voor de laatste nieuwtjes.
+          </p>
+          <p>
+            Tickets zijn nu beschikbaar via de website van A-Eskwadraat!
+          </p>
+          <p>
+            <a href="https://www.a-eskwadraat.nl/Activiteiten/bmn/7693/BtaMusicNight2019/KaartjeKopen?cleanhtml=1">
+              <Button bsStyle="primary" bsSize="lg">Tickets</Button>
+            </a>
           </p>
         </Col>
       </Grid>
